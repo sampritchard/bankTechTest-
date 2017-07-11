a199abca9238180bd27b4e9321a3f065da21ec45
Sam Pritchard <sampritchard@msn.com>
2017-07-11 13:30:35 +0100

    Pushes deposit into transaction history array and passes test

--- spec/TransactionsSpec.js
+++ spec/TransactionsSpec.js
@@ -1,13 +1,17 @@
 describe("Transaction", function() {
-  var transaction;
   var account;
 
   beforeEach(function() {
-    transaction = new Transaction();
     account = new Account();
   });
 
   it("should start with 0 transactions", function() {
     expect(transaction.history).toEqual([]);
   });
+
+  it("should update when a account deposit is made", function() {
+    account.deposit(1000);
+    account.deposit(1000);
+    expect(transaction.history).toEqual([1000,1000])
+  })
 });
--- src/Account.js
+++ src/Account.js
@@ -1,9 +1,11 @@
 function Account(balance=0) {
+  transaction = new Transaction();
   this.balance = balance
 }
 
 Account.prototype.deposit = function (number) {
   this.balance += number
+  transaction.history.push(number);
 };
 
 Account.prototype.withdraw = function (number) {
